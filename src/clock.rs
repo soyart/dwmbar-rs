25e58e3d03e90d0e773fab20a16acd4f2dac043a
soyart <artdrawin@gmail.com>
2026-01-25 01:08:52 +0700

    replace crate chrono with crate time

--- src/clock.rs
+++ src/clock.rs
@@ -1,5 +1,13 @@
-use chrono::Local;
+use time::OffsetDateTime;
+use time::format_description;
 
 pub(crate) fn get() -> String {
-    Local::now().format("%A, %b %d > %H:%M").to_string()
+    let format = format_description::parse(
+        "[weekday], [month repr:short] [day padding:zero] > [hour repr:24 padding:zero]:[minute padding:zero]"
+    ).unwrap();
+    
+    OffsetDateTime::now_local()
+        .unwrap()
+        .format(&format)
+        .unwrap()
 }
